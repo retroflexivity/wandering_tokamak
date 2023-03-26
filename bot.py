3f446a05d8820c16bbbd20f26b1e56adf4c04cc7
retroflexivity <ark.shaldov@gmail.com>
2023-03-26 14:48:54 +0500

    several improvements

--- bot.py
+++ bot.py
@@ -1,25 +1,38 @@
+# бот, постящий в канал 
+
 import telebot
-import sched, time
+# import sched, time
+import threading
 from generator import Generator
 
+
 token = open('tgbot_token.txt').read()
+
+# вот этот — https://t.me/wandering_tokamak
 chat_id = '@wandering_tokamak'
 bot = telebot.TeleBot(token)
 
-def send_to_channel(scheduler):
+# функция, отправляющая в канал
+def send_to_channel():
     print('started')
+    # пытаемся пока не получится
     while True: 
         try:
+            # генерируем картинку
             gen = Generator()
             image = gen.generate()
             print('generated')
+
+            # отправляем в канал
             bot.send_photo(chat_id=chat_id, photo=image)
             print('sent')
+            break
         except Exception as e:
             print(e)
-    scheduler.enter(60, 1, send_to_channel, (scheduler,))
+            # raise e
+    
+    # спим...
+    print('sleeping...')
+    threading.Timer(600.0, send_to_channel).start()
 
-
-sch = sched.scheduler(time.time, time.sleep)
-sch.enter(60, 1, send_to_channel, (sch,))
-sch.run()
+send_to_channel()
--- generator.py
+++ generator.py
@@ -1,9 +1,9 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
-import random
+from numpy.random import choice
+from numpy import array
 import torch
 import torch.nn.functional as F
 import pandas as pd
-from random import choice
 
 from io import BytesIO
 from PIL import Image
@@ -24,10 +24,12 @@ class Generator():
     line_size = round(image_width / font_size * 1.9)
     
     font = ImageFont.truetype('impact.ttf', size=font_size)
-    text_colors = ((254 ,253, 3), (0, 0, 0), (255, 255, 255))
+    text_colors = {'yellow': (254 ,253, 3), 'black': (0, 0, 0), 'white': (255, 255, 255)}
+    text_color_freq = (0.75, 0.2, 0.05)
     stroke_width = 2
     stroke_fill = 'black'
 
+    # гпт-генератор
     def gptate(
             self,
             model,
@@ -77,6 +79,7 @@ class Generator():
                     
         return output_text
 
+    # добавление разделителей строк, чтобы не залезали за край
     def split_paragraphs(self, text, line_size=line_size):
         paragraphs = ['']
         for word in text.split(' '):
@@ -87,30 +90,36 @@ class Generator():
 
         return '\n'.join(paragraphs)[1:]
 
+    # генерация текста вообще
     def generate_text(self):
-        text = self.gptate(model.to('cpu'), tokenizer, 'токамак-странник говорит: ')
-        text = text.replace('токамак-странник говорит: ', '').split('<|')[0].upper()
+        text = self.gptate(model.to('cpu'), tokenizer, ' ').upper()
         try:
-            text = text[text.index(' ') + 1:text.rindex('.')]
+            # обрезаем до последнего законченного
+            text = text['''text.index(' ') + 1''':text.rindex('.')]
         except:
             pass
         return self.split_paragraphs(text)
         
+    # достаем картинку по запросу
     def get_image(self, image_text):
-        text_color = choice(self.text_colors)
-
         search = self.unspl.search(type_='photos', per_page=50, query=image_text)
         entry = next(search.entries)
         image = Image.open(BytesIO(requests.get(entry.link_download).content))
         return entry.id, image.resize((self.image_width, round(image.size[1] / image.size[0] * self.image_width)))
 
+    # генерируем
     def generate(self):
+        # мы не хотим, чтобы тексты или картинки повторялись
         prev = pd.read_csv('bayan.csv')
 
+        print('generating text')
         text = self.generate_text()
+        
+        print('generating image')
         image_id, image = self.get_image(text)
     
         if (text[:20] in prev['text'].unique()) or (image_id in prev['image'].unique()):
+            print('duplicate!')
             return self.generate()
         else:
             pd.concat((
@@ -123,8 +132,9 @@ class Generator():
         draw = ImageDraw.Draw(image)
         _, _, w, h = draw.textbbox((0, 0), text, font=self.font)
         
+        text_color = self.text_colors[choice(list(self.text_colors.keys()), 1, p=self.text_color_freq)[0]]
         draw.text(((self.image_width - w) / 2, (image_height - h) / 20), text=text, 
-                    fill=self.text_color, font=self.font, align='center', 
+                    fill=text_color, font=self.font, align='center', 
                     stroke_width=self.stroke_width, stroke_fill=self.stroke_fill)
         
         return image
